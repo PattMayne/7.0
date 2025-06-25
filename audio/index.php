754ec072ee63c13858f123a7ea4f1f6de1aadbb9
Matt Payne <pattmayne@gmail.com>
2025-06-25 17:35:55 -0400

    smaller image

--- audio/index.php
+++ audio/index.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="pragma" content="no-cache" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>pattmayne.com AUDIO</title>
+    <title>AUDIO pattmayne.com</title>
     <link rel="icon" href="../img/guy_icon.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/foundation.css">
     <link rel="stylesheet" href="../css/app.css?ts=<?=time()?>">
