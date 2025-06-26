e82661e0b88263bab384fa628742e821d81d36a2
Matt Payne <pattmayne@gmail.com>
2025-06-26 08:09:14 -0400

    new post

---
+++ blog/posts/2025-06-26-puerh-2008-reserve.php
@@ -0,0 +1,26 @@
+
+
+
+<div id="article_title">Puerh Shou 2008 Reserve</div>
+<div id="article_date">2025-06-26</div>
+<div id="categories">tea, puerh</div>
+
+<article class="article clearfix" id="article_body">
+
+    <!-- START OF BODY TO REPLACE -->
+
+    <img src="posts/img/puerh-2008-reserve-1.png" alt="Can you see the floral overtones? The musky undertones?" style="float: left; width: 40%; margin-right: 10px;" />
+    <p>
+        This is what I want from a puerh tea. The musk is muted and mellow, you can taste the fermentation with a floral backdrop. It's richly evened out, flavorful as broth but not overpowering. And with 17 years of aging the tea itself is not bitter. There's nothing sharp about this. It's a beautiful and delicious tea. Not much hint of mushroom or forest floor. I mean, they're there, but the floral sweetness is more prominent. This might be my favorite tea. It certainly rivals the 2004 vintage I used to get (my vendor ran out).
+    </p>
+
+    <p>
+        It's easy to taste the difference between the regular 2008 batch and this "reserve" batch. Robustly organic, yet elgant and delicate. This is my new king.
+        <img src="posts/img/puerh-2008-reserve-2.png" alt="Loose puerh tea" style="float: right; width: 200px; margin: 0 0 10px 10px;" />
+    </p>
+
+
+    <!-- END OF BODY TO REPLACE -->
+
+
+</article>
