fd7032d960354ee1ab03bb7f890ac0d4dd73c5c1
Matt Payne <pattmayne@gmail.com>
2025-06-26 16:44:31 -0400

    another post (2k reserve)

---
+++ blog/posts/2025-06-26-puerh-2000-reserve.php
@@ -0,0 +1,40 @@
+
+<!--
+
+    TEMPLATE for a blog post.
+    The blog posts in this folder will all be snippets for inclusion in the blog post list, and to be display in-full in the post.php file.
+
+    NAMING CONVENTION:
+    * YYYY-MM-DD-post-title-slug.php
+
+    Text can wrap around inline images. Use inline styling as depicted below.
+
+    post.php is rendering this. So image links must be relative to post.php
+
+-->
+
+<div id="article_title">Puerh Shou 2000 Imperial Reserve</div>
+<div id="article_date">2025-06-26</div>
+<div id="categories">review, tea, puerh</div>
+
+<article class="article clearfix" id="article_body">
+
+    <!-- START OF BODY TO REPLACE -->
+
+    <img src="posts/img/puerh-2000-1.png" alt="Put the swamp in your soul" style="float: left; width: 40%; margin-right: 10px;" />
+    <p>
+        This is a very nice balance between the beetjuice flavor of the 2003 reserve, with the fermented floral depths of the exquisite 2008 reserve. It's fruity, dark, and layered. I keep going back for another sip because my tongue wants to explore the hidden aspects of this exotic bouquet. A fantastic example of puerh's inexhaustible diversity of flavors.
+    </p>
+
+    <p>
+        Is it a bit too sharp and sweet for a daily morning tea? Possibly. But just barely. It's a delight to drink.
+        <img src="posts/img/puerh-2000-2.png" alt="The dirt from which flavor grows" style="float: right; width: 200px; margin: 0 0 10px 10px;" />
+        Leaning toward the floral (like beets and roses) while the dark forest-floor aspect takes a back seat and provides a basement for the more-dominant flavors. There isn't much mushroom in this murky broth. But there is a ton of flavor.
+
+    </p>
+
+
+    <!-- END OF BODY TO REPLACE -->
+
+
+</article>
