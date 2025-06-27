544ae3748dc43900281846d0a2d6bf91666638a8
Matt Payne <pattmayne@gmail.com>
2025-06-27 16:07:27 -0400

    new review

---
+++ blog/posts/2025-06-27-puerh-1991.php
@@ -0,0 +1,28 @@
+
+
+
+<div id="article_title">Puerh 1991 Reserve</div>
+<div id="article_date">2025-06-27</div>
+<div id="categories">tea, puerh, review</div>
+
+<article class="article clearfix" id="article_body">
+
+    <!-- START OF BODY TO REPLACE -->
+
+    <p>Another unique <i>shou.</i></p>
+    <img src="posts/img/puerh-1991-reserve-1.png" alt="Dark as Darth Vader's soul" style="float: left; width: 40%; margin-right: 10px;" />
+    <p>
+        Like Darth Vader, this drink is more fermentation than tea. That's not entirely a bad thing. The musk isn't too pungent. Where some other puerh's have a fruity or floral aspect to balance the dank, this one packs some pepper. It's the only one I've tried that I'd call <i>spicy.</i> It's tasty. Not exactly <i>delicious,</i> but certainly enjoyable.
+    </p>
+
+    <p>
+        The earthy notes are muted. It's not like drinking the forest floor, but instead like fermented Dr. Pepper. There aren't a lot of layers. This has fewer notes, so they shine through more clearly.
+        <img src="posts/img/puerh-1991-reserve-2.png" alt="loose puerh tea" style="float: right; width: 200px; margin: 0 0 10px 10px;" />
+        Aged since 1991, this is the oldest tea I've tried so far. Maybe that's why I can't taste much tea behind the tang. It faded away, like Anakin Skywalker's humanity. Leaving behind nothing but the peppery darkness. But that darkness is why we keep coming back.
+    </p>
+
+
+    <!-- END OF BODY TO REPLACE -->
+
+
+</article>
