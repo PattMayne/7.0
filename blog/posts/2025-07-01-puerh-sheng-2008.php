c8c8c1be21639f743c37cc5092cce1d13f1a36a8
Matt Payne <pattmayne@gmail.com>
2025-07-01 17:23:03 -0400

    sheng review

---
+++ blog/posts/2025-07-01-puerh-sheng-2008.php
@@ -0,0 +1,39 @@
+
+<!--
+
+    TEMPLATE for a blog post.
+    The blog posts in this folder will all be snippets for inclusion in the blog post list, and to be display in-full in the post.php file.
+
+    NAMING CONVENTION:
+    * YYYY-MM-DD-post-title-slug.php
+
+    Text can wrap around inline images. Use inline styling as depicted below.
+
+    post.php is rendering this. So image links must be relative to post.php
+
+-->
+
+<div id="article_title">Puerh Sheng Lincang 2008</div>
+<div id="article_date">2025-07-01</div>
+<div id="categories">tea, puerh, sheng, review</div>
+
+<article class="article clearfix" id="article_body">
+
+    <!-- START OF BODY TO REPLACE -->
+
+    <img src="posts/img/puerh-sheng-2008-1.png" alt="Golden red puerh sheng tea" style="float: left; width: 40%; margin-right: 10px;" />
+    <p>
+        Welcome to flavor country. This puerh <i>sheng</i> has been fermenting naturally since 2008. Its musk is deep, not like forest floor or mushroom (which define my favorite puerh <i>shou</i> varieties), it's a dry-dank flavor like aged apple cider, <i>sans tartness</i>, with undertones of white wine. My other favorite puerh's are dark in color and flavor, but this is soft and delicate like a white tea, beneath the refined pungent fermentation, and just packed with layers of flavor.
+    </p>
+
+    <p>
+        It's really a delicious tea. The natural fermentation is totally distinct from the "artificial" fermentation I'm used to from the shou varieties. Those are more dark, more in-your-face.
+        <img src="posts/img/puerh-sheng-2008-2.png" alt="Loose puerh sheng tea leaves" style="float: right; width: 200px; margin: 0 0 10px 10px;" />
+        The combination of delicate subtlety and tongue-dominating pungent dankness makes this a novelty where each layer of flavor is delicious. Truly a prestigious and exotic tea.
+    </p>
+
+
+    <!-- END OF BODY TO REPLACE -->
+
+
+</article>
