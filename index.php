64b859e6b62a88dee3e862c3eed7b4fb2b1c94cd
Matt Payne <pattmayne@gmail.com>
2026-03-17 11:10:43 -0400

    smaller subimages

--- index.php
+++ index.php
@@ -35,13 +35,18 @@
                         <img src="img/matt-1-YELLOW.png" id="profile-pic">
                     </div>
                     <div class="small-3 large-6 medium-6 cell p-p-cell-R hide-for-small-only">
-                        <img src="img/matt-1-GRAY.png" id="profile-pic">
+                        <img src="img/matt-1-BLUE.png" id="profile-pic">
                     </div>
                     <div class="small-3 large-6 medium-6 cell p-p-cell-L hide-for-small-only">
                         <img src="img/matt-1-RED.png" id="profile-pic">
                     </div>
                     <div class="small-3 large-6 medium-6 cell p-p-cell-R hide-for-small-only">
-                        <img src="img/matt-1-BLUE.png" id="profile-pic">
+                        <div class="inner-img-grid">
+                            <img src="img/matt-1-YELLOW.png" class="tiny-img">
+                            <img src="img/matt-1-RED.png" class="tiny-img">
+                            <img src="img/matt-1-BLUE.png" class="tiny-img">
+                            <img src="img/matt-1-GRAY.png" class="tiny-img">
+                        </div>
                     </div>
 
 
